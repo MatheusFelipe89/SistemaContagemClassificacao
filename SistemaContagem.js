1dda9e00a1d4d8626d5f3608f586dc87201bf183
matheus felipe <kadoshimatheus@gmail.com>
2021-09-02 00:25:52 -0300

    first commit

---
+++ SistemaContagem.js
@@ -0,0 +1,34 @@
+let NumeroDeAlunos = 10;
+
+for (let contador = 1; contador <= NumeroDeAlunos; contador++) {
+   // console.log(contador)
+
+if (contador === 0) {
+    console.log("O numero atual é ZERO");
+} else if (contador % 2 == 0) {
+    console.log("O numero " + contador + " é PAR")
+} else {
+    console.log("O numero " + contador + " é IMPAR")
+}
+}
+/*
+AND (E)
+true && true -> true
+true && false -> false
+false && true -> false
+false && false -> False
+
+OR (OU)
+true || true -> true
+true || false -> true
+false || true -> true
+false || false -> false
+
+NOT (NÃO)
+!true -> false
+!false -> true
+
+
+
+
+*/
